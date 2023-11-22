0f476c97e2882ab8042d1969c2e13b59c94f4ca4
Anna Thompson <annathompsonw@gmail.com>
2023-11-21 21:59:51 -0700

    Added photo upload options to recipe details

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 import "package:provider/provider.dart";
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,6 +16,7 @@ class MyApp extends StatelessWidget {
       create: (context) => MyAppState(),
       child: MaterialApp(
         title: "Recipe Book App",
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(
@@ -31,11 +34,16 @@ class MyAppState extends ChangeNotifier {
     Recipe(name: "Ravioli"),
     Recipe(name: "Spaghetti"),
     Recipe(name: "Grilled Chicken"),
-    Recipe(name: "Pizza"),
+    Recipe(name: "Pizza", description: "Pizza!"),
   ];
 
   void addRecipe(Recipe recipe) {
     recipes.add(recipe);
+    notifyListeners();
+  }
+
+  void deleteRecipe(Recipe recipe) {
+    recipes.remove(recipe);
     notifyListeners();
   }
 }
@@ -116,8 +124,7 @@ class _MyHomePageState extends State<MyHomePage> {
 class Recipe {
   String name;
   String description;
-  //List<String> ingredients = [];
-  //List<String> directions = [];
+  String? imagePath;
 
   Recipe({String name = "Untitled Recipe", String description = ""})
       : this.name = name,
@@ -189,7 +196,8 @@ class RecipesPage extends StatelessWidget {
           crossAxisCount: 2,
           crossAxisSpacing: 8.0,
           mainAxisSpacing: 8.0,
-          childAspectRatio: 0.8, // Adjust the aspect ratio to make the cards longer vertically
+          childAspectRatio:
+              0.8, // Adjust the aspect ratio to make the cards longer vertically
         ),
         itemCount: recipes.length,
         itemBuilder: (context, index) {
@@ -200,22 +208,156 @@ class RecipesPage extends StatelessWidget {
   }
 }
 
-class RecipeDetailsPage extends StatelessWidget {
+class RecipeDetailsPage extends StatefulWidget {
   final Recipe recipe;
 
   RecipeDetailsPage(this.recipe);
 
   @override
+  State<RecipeDetailsPage> createState() => _RecipeDetailsPageState();
+}
+
+class _RecipeDetailsPageState extends State<RecipeDetailsPage> {
+  File? _imageFile;
+
+  Future<void> _pickImage(ImageSource source) async {
+    try {
+      final pickedFile = await ImagePicker().pickImage(source: source);
+      if (pickedFile != null) {
+        setState(() {
+          _imageFile = File(pickedFile.path);
+          widget.recipe.imagePath = pickedFile.path;
+        });
+      }
+    } catch (e) {
+      print('Error picking image: $e');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    //var recipeName = recipe.name ?? "Intitled Recipe"; // Use a default value if name is null
     return Scaffold(
-      appBar: AppBar(
-        title: Text(recipe.name),
-      ),
-      body: Center(
-        child: Text("Details for $recipe"),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: _imageFile != null ? 300.0 : 0,
+            floating: false,
+            pinned: true,
+            flexibleSpace: Stack(
+              children: [
+                // Background Image
+                if (_imageFile != null)
+                  Image.file(
+                    _imageFile!,
+                    height: 300.0,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                // Popup Menu Button
+                Positioned(
+                  top: 6,
+                  left: 0,
+                  right: 0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      PopupMenuButton<String>(
+                        onSelected: handleClick,
+                        itemBuilder: (BuildContext context) {
+                          return {
+                            "Upload Photo",
+                            "Take Photo",
+                            "Delete Recipe"
+                          }.map((String choice) {
+                            return PopupMenuItem<String>(
+                              value: choice,
+                              child: Text(choice),
+                            );
+                          }).toList();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              // Recipe Title
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  widget.recipe.name,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              // Recipe Description
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  widget.recipe.description,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              // Other recipe details can be displayed here
+            ]),
+          ),
+        ],
       ),
     );
+  }
+  
+  void handleClick(String value) async {
+    switch (value) {
+      case "Upload Photo":
+        await _pickImage(ImageSource.gallery);
+        break;
+      case "Take Photo":
+        await _pickImage(ImageSource.camera);
+        break;
+      case "Delete Recipe":
+        _deleteRecipe();
+        break;
+    }
+  }
+
+  void _deleteRecipe() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Are you sure?"),
+          content: Text("This action will permanently delete the recipe."),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+              },
+              child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+                _performDelete(); // Perform the actual delete
+              },
+              child: Text("Delete"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _performDelete() {
+  var appState = context.read<MyAppState>();
+
+  appState.deleteRecipe(widget.recipe);
+  Navigator.pop(context); // Navigate back to the previous screen
   }
 }
 
